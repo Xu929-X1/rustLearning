3e703566b9279aeae7b70cf31a9cfada39a5f522
Bowei Xu <boweixu929@gmail.com>
2023-10-23 21:58:07 -0400

    reformat the code

--- txt-editor/src/main.rs
+++ txt-editor/src/main.rs
@@ -1,28 +1,6 @@
-use std::{io::{self, stdout, Read, Error}, process};
-use termion::raw::IntoRawMode;
-
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111 //control sets the upper 3 bits to 0
-}
-
-fn die(e: Error){
-    panic!(e);
-}
-
+mod editor;
+use editor::Editor;
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b)=>{
-                let c = b as char;
-                if c.is_control(){
-                    println!("{:?} \r", c);
-                }else {
-                    println!("{:?} ({}) \r", b, c )
-                }
-            }
-            Err(err)=>die(err)
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
