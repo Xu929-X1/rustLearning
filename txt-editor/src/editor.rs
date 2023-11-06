0c9275a093a7ba063095d9fc5efa1bedb8754ef7
Bowei Xu <boweixu929@gmail.com>
2023-11-06 12:19:57 -0500

    some updates

--- txt-editor/src/editor.rs
+++ txt-editor/src/editor.rs
@@ -1,11 +1,14 @@
+use crate::Document;
+use crate::Row;
 use crate::Terminal;
 use termion::event::Key;
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: CursorPosition,
+    document: Document,
 }
-
+#[derive(Default)]
 pub struct CursorPosition {
     pub x: u16,
     pub y: u16,
@@ -35,13 +38,14 @@ impl Editor {
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            cursor_position: CursorPosition { x: 0, y: 0 },
+            cursor_position: CursorPosition::default(),
+            document: Document::open(),
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&CursorPosition { x: 0, y: 0 });
+        Terminal::cursor_position(&CursorPosition::default());
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye. \r");
@@ -53,11 +57,20 @@ impl Editor {
         Terminal::flush()
     }
 
+    pub fn draw_row(&self, row: &Row) {
+        let start = 0;
+        let end = self.terminal.size().width as usize;
+        let row = row.render(start, end);
+        println!("{}", row);
+    }
+
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_row();
-            if row == height / 3 {
+            if let Some(row) = self.document.row(terminal_row as usize) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
             } else {
                 println!("~\r");
@@ -80,7 +93,14 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('c') => self.should_quit = true,
-            Key::Up | Key::Down | Key::Right | Key::Left => self.move_cursor(pressed_key),
+            Key::Up
+            | Key::Down
+            | Key::Right
+            | Key::Left
+            | Key::PageDown
+            | Key::PageUp
+            | Key::End
+            | Key::Home => self.move_cursor(pressed_key),
             _ => (),
         }
         Ok(())
@@ -88,11 +108,26 @@ impl Editor {
 
     fn move_cursor(&mut self, key: Key) {
         let CursorPosition { mut x, mut y } = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1);
+        let width = size.width.saturating_sub(1);
         match key {
             Key::Up => y = y.saturating_sub(1),
-            Key::Down => y = y.saturating_add(1),
+            Key::Down => {
+                if y < height {
+                    y.saturating_add(1);
+                }
+            }
             Key::Left => x = x.saturating_sub(1),
-            Key::Right => x = x.saturating_add(1),
+            Key::Right => {
+                if x < width {
+                    x.saturating_add(1);
+                }
+            }
+            Key::PageDown => y = height,
+            Key::PageUp => y = 0,
+            Key::Home => x = 0,
+            Key::End => x = width,
             _ => (),
         }
         self.cursor_position = CursorPosition { x, y }
--- txt-editor/src/main.rs
+++ txt-editor/src/main.rs
@@ -1,8 +1,12 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
 mod terminal;
+mod document;
+mod row;
 pub use terminal::Terminal;
 pub use editor::CursorPosition;
+pub use document::Document;
+pub use row::Row;
 use editor::Editor;
 fn main() {
    Editor::default().run();
