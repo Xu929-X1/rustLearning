516f94eca2b5cbd43d992c54c1ce0da69316e5dd
Bowei Xu <boweixu929@gmail.com>
2023-11-28 19:19:46 -0500

    Finished not persistable insert

--- txt-editor/src/document.rs
+++ txt-editor/src/document.rs
@@ -1,7 +1,6 @@
 use std::fs;
-
 use crate::Row;
-
+use crate::Position;
 #[derive(Default)]
 pub struct Document {
     rows: Vec<Row>,
@@ -17,6 +16,17 @@ impl Document {
         }
 
         Ok(Self { rows, file_name: Some(filename.to_string()) })
+    }
+
+    pub fn insert(&mut self, at: &Position, c:char){
+        if at.y == self.len(){
+            let mut row = Row::default();
+            row.insert(0, c);
+            self.rows.push(row);
+        }else if at.y < self.len(){
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.insert(at.x, c);
+        }
     }
 
     pub fn row(&self, index: usize) -> Option<&Row> {
